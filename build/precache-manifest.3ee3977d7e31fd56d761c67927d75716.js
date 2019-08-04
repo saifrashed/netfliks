0e7993a6acf3a0437fe5deac07b729ead91b2fe0
Saif <saifeddinerashed@icloud.com>
2019-08-04 13:53:25 +0200

    Added styling and interactivity improvements

---
+++ build/precache-manifest.3ee3977d7e31fd56d761c67927d75716.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e51779722eff70b861069070dadb4b48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6f6db5f7e4e9d8be1d6",
+    "url": "/static/css/main.cfaa0051.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "b6f6db5f7e4e9d8be1d6",
+    "url": "/static/js/main.0d2e0b9b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
